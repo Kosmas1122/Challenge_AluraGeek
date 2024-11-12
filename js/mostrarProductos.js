cfaeaa2875b7d0069bde4acd6f45e949804bb258
David-Zephyrus <dbekosmas@gmail.com>
2024-11-11 20:34:35 -0500

    Cambios en contenedor de la funcion crearCard:
    Se modifica el contenedor que había en producto.innerHTML, donde
    se elimina el contenedor externo <div class="card"> y se deja
    solo su contenido con la clase card.
    Se hacen modificaciones en la hoja de estilos para fijar header y
    footer.

--- js/mostrarProductos.js
+++ js/mostrarProductos.js
@@ -5,9 +5,18 @@ const lista = document.querySelector("[data-lista]");
 
 function crearCard(nombre, precio, imagen) {
     const producto = document.createElement("li");
-    producto.className = "productos__item";
+    producto.className = "card";
     producto.innerHTML = 
-    `<div class="card">
+    `<img class="card__img" src="${imagen}" alt="Imagen producto" />
+    <div class="card__info">
+        <p> ${nombre} </p>
+        <div class="card__valor">
+            <p> ${precio} </p>
+            <button class="card__boton"> <div class="card__trash"></div> </button>
+        </div>
+    </div>`;
+    
+    /* `<div class="card">
         <img class="card__img" src="${imagen}" alt="Imagen producto" />
         <div class="card__info">
             <p> ${nombre} </p>
@@ -16,8 +25,8 @@ function crearCard(nombre, precio, imagen) {
                 <button class="card__boton"> <div class="card__trash"></div> </button>
             </div>
         </div>
-    </div>`;
-    
+    </div>`; */
+
     return producto;
 } 
 
