810c10e9bf86b061be6a3945eecec803ae6e14fb
David-Zephyrus <dbekosmas@gmail.com>
2024-11-14 21:43:19 -0500

    Requisición DELETE OK:
    Se crea función borrarProducto() para la eliminación de productos
    en la aplicación.

--- js/mostrarProductos.js
+++ js/mostrarProductos.js
@@ -3,18 +3,21 @@ import {conexionAPI} from "./conexionAPI.js";
 const lista = document.querySelector("[data-lista]");
 
 
-function crearCard(nombre, precio, imagen) {
+function crearCard(id, nombre, precio, imagen) {
     const producto = document.createElement("li");
     producto.className = "card";
+    /* producto.id = id; */
+
     producto.innerHTML = 
     `<img class="card__img" src="${imagen}" alt="Imagen producto" />
     <div class="card__info">
         <p> ${nombre} </p>
         <div class="card__valor">
             <p> ${precio} </p>
-            <button class="card__boton"> <div class="card__trash"></div> </button>
+            <button class="card__boton" onclick="borrarProducto('${id}')"> <div class="card__trash"></div> </button>
         </div>
     </div>`;
+
     
     return producto;
 } 
@@ -23,7 +26,8 @@ function crearCard(nombre, precio, imagen) {
 async function listarProductos(){
     const listaAPI = await conexionAPI.listarProductos();
 
-    listaAPI.forEach(producto => lista.appendChild(crearCard(producto.nombre, producto.precio, producto.imagen)));
+    /* listaAPI.forEach(producto => lista.appendChild(crearCard(producto.nombre, producto.precio, producto.imagen))); */
+    listaAPI.forEach(producto => lista.appendChild(crearCard(producto.id, producto.nombre, producto.precio, producto.imagen)));
 }
 
 listarProductos();
